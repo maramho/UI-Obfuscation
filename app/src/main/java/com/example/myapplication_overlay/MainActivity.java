0ef90292237a3d7273e447d21ccfb7cdf09eadb1
dongho <ehdghrof9@gmail.com>
2023-03-02 14:49:51 +0900

    fist upload

---
+++ app/src/main/java/com/example/myapplication_overlay/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.myapplication_overlay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final int ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE); // 스크린샷 방지하는 커멘드
+        getClassLoader();
+
+
+
+        Button bt_start = (Button) findViewById(R.id.bt_start);
+        bt_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkPermission();
+            }
+
+            public ClassLoader getClassLoader(){
+
+                return getClassLoader();
+            }
+        });
+
+        Button bt_stop = (Button) findViewById(R.id.bt_stop);
+        bt_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MyService.class));
+            }
+
+
+
+
+        });
+    }
+
+    public void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {   // 마시멜로우 이상일 경우
+            if (!Settings.canDrawOverlays(this)) {              // 체크
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE);
+            } else {
+                startService(new Intent(MainActivity.this, MyService.class));
+            }
+        } else {
+            startService(new Intent(MainActivity.this, MyService.class));
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE) {
+            if (!Settings.canDrawOverlays(this)) {
+
+
+            } else {
+                startService(new Intent(MainActivity.this, MyService.class));
+            }
+        }
+    }
+
+}
+
+    
+    
